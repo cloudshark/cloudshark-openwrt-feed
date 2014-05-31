d5ef5c672b4f3d5c4b0e1780b2b492e56a8d0f88
Petar Koretic <petar.koretic@sartura.hr>
2014-05-31 20:26:41 +0200

    cshark-luci: implement list support
    Adds ability to get and delete list from uci. Also fixes multiple open
    windows and gateway timeout bugs.
    
    Signed-off-by: Petar Koretic <petar.koretic@sartura.hr>

--- cshark-luci/files/controller/cshark.lua
+++ cshark-luci/files/controller/cshark.lua
@@ -24,6 +24,15 @@ function index()
 
 		page = entry({"admin", "network", "cshark_iface_dump_stop"}, call("cshark_iface_dump_stop"), nil)
 		page.leaf = true
+
+		page = entry({"admin", "network", "cshark_check_status"}, call("cshark_check_status"), nil)
+		page.leaf = true
+
+		page = entry({"admin", "network", "cshark_link_list_get"}, call("cshark_link_list_get"), nil)
+		page.leaf = true
+
+		page = entry({"admin", "network", "cshark_link_list_clear"}, call("cshark_link_list_clear"), nil)
+		page.leaf = true
 end
 
 function cshark_iface_dump_start(ifname, value, flag, filter)
@@ -37,27 +46,59 @@ function cshark_iface_dump_start(ifname, value, flag, filter)
 		filter = ''
 	end
 
-	luci.http.prepare_content("text/html")
+	luci.http.prepare_content("text/plain")
 
-	local res = io.popen("/sbin/cshark -i " .. ifname .. " -" .. flag .. " " .. value .. " -p /tmp/cshark-luci.pid " .. filter .. " 2>&1")
-	if res then
-		while true do
-			local ln = res:read("*l")
-			if not ln then break end
-			luci.http.write(ln)
-			luci.http.write("\r\n")
-		end
-		res:close()
-	end
+	local res = os.execute("/sbin/cshark -i " .. ifname .. " -" .. flag .. " " .. value .. " -p /tmp/cshark-luci.pid " .. filter .. " 2>&1 &")
+	luci.http.write(tostring(res))
 end
 
 function cshark_iface_dump_stop()
-	luci.http.prepare_content("text/html")
+	luci.http.prepare_content("text/plain")
 
 	local f = io.open("/tmp/cshark-luci.pid", "rb")
 	local pid = f:read("*all")
 	f:close()
 
-	local res = os.execute("kill -INT " ..pid)
+	local res = os.execute("kill -INT " .. pid)
 	luci.http.write(tostring(res))
+end
+
+function cshark_check_status()
+	luci.http.prepare_content("text/plain")
+
+	local f = io.open("/tmp/cshark-luci.pid","r")
+	if f ~= nil then
+		io.close(f)
+		luci.http.write("1")
+	else
+		luci.http.write("0")
+	end
+end
+
+function cshark_link_list_get()
+	local uci = require("uci").cursor()
+
+	luci.http.prepare_content("application/json")
+
+	luci.http.write("[")
+
+	local t = uci:get("cshark", "cshark", "entry")
+	local n = 0
+	for i = #t, 1, -1 do
+		n = n + 1
+		if n > 10 then break end
+
+		luci.http.write("[\"" .. t[i] .. "\"],")
+	end
+
+	luci.http.write("[]]")
+end
+
+function cshark_link_list_clear()
+	local uci = require("uci").cursor()
+
+	uci:delete("cshark", "cshark", "entry")
+	uci:commit("cshark");
+
+	luci.http.status(200, "OK")
 end
